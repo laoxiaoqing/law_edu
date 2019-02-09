88e3156e34065ae76b31d1f1b63b672b81808bd4
laoxiaoqing <786691202@qq.com>
2019-02-09 21:33:40 +0800

    slidingMenu插件

--- app/src/main/java/com/example/administrator/lawapp/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/lawapp/activity/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.administrator.lawapp.R;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //1.设置主页面
         setContentView(R.layout.activity_main);
+        //
     }
 }
