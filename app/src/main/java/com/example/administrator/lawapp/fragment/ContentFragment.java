e6f1aee33bdcc8c49d38c2c42589228a351915b8
laoxiaoqing <786691202@qq.com>
2019-02-10 15:33:02 +0800

    主页Fragment布局

--- app/src/main/java/com/example/administrator/lawapp/fragment/ContentFragment.java
+++ app/src/main/java/com/example/administrator/lawapp/fragment/ContentFragment.java
@@ -1,6 +1,9 @@
 package com.example.administrator.lawapp.fragment;
 
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.administrator.lawapp.R;
@@ -9,10 +12,14 @@ import com.example.administrator.lawapp.utils.LogUtil;
 
 public class ContentFragment extends BaseFragment {
     private TextView textView;
+    private ViewPager viewPager;
+    private RadioGroup rg_main;
     @Override
     public View initView() {
         LogUtil.e("正文Fragment视图初始化了");
         View view = View.inflate(context, R.layout.content_fragment,null);
+        viewPager = (ViewPager)view.findViewById(R.id.viewpager);
+        rg_main = (RadioGroup) view.findViewById(R.id.rg_main);
         return view;
     }
 
@@ -20,6 +27,6 @@ public class ContentFragment extends BaseFragment {
     public void initData() {
         super.initData();
         LogUtil.e("正文Fragment数据被初始化了");
-
+        rg_main.check(R.id.rb_home);
     }
 }
